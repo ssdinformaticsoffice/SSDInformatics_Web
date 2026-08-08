0de4f3aa4106aeb7f1d90dd74a6262bab9d1d66f
Khushi40222 <khushimy2006@gmail.com>
2026-08-08 18:15:34 +0530

    Remove old admin folder

--- frontend/src/Admin/pages/ManageAbout.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ManageAbout = () => {
-  return (
-    <div>ManageAbout</div>
-  )
-}
-
-export default ManageAbout
--- frontend/src/Admin/pages/ManageServices.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ManageServices = () => {
-  return (
-    <div>ManageServices</div>
-  )
-}
-
-export default ManageServices
